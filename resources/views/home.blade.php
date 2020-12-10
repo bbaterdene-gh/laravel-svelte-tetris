aec904c2d93b485c714ab23a99e7ce52050abc2f
BBat-Erdene <>
2020-12-10 21:43:37 +0900

    nav fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card text-white bg-dark mb-3">
+                <div class="card-header">{{ __('Тоглолтын түүх') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))

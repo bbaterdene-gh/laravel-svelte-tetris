db484aef34c8d4000a2942d8e34fbeb1b2056a15
BBat-Erdene <>
2020-12-10 21:27:47 +0900

    log in to play

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,17 +42,15 @@
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
                 <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline btn-flat-border">Home</a>
+                <div id="svelte-app"></div>
             @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline btn-flat-border">Login</a>
 
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                  <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline btn-flat-border">Register</a>
                 @endif
             @endif
         </div>
       @endif
-      <div id="svelte-app">
-
-      </div>
     </body>
 </html>

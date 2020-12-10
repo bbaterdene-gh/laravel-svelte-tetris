ac2695dc4d70450954795b8410694247f409740d
BBat-Erdene <>
2020-12-10 22:36:21 +0900

    added point resource

---
+++ database/migrations/2020_12_10_133025_create_points_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->integer('point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
